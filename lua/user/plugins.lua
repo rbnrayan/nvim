ad23e59fc063fd73616a7b444b996b0d0d8102f6
rbnrayan <rayan.robion7@gmail.com>
2022-05-20 17:17:25 +0200

    Telescope

--- lua/user/plugins.lua
+++ lua/user/plugins.lua
@@ -66,6 +66,9 @@ return packer.startup(function(use)
   use "L3MON4D3/LuaSnip" --snippet engine
   use "rafamadriz/friendly-snippets" -- a bunch of snippets to use
 
+  -- Telescope
+  use "nvim-telescope/telescope.nvim"
+
   -- Automatically set up your configuration after cloning packer.nvim
   -- Put this at the end after all plugins
   if PACKER_BOOTSTRAP then
