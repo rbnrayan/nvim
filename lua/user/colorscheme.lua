9ae36526afaa3b90a916c673ebd7761e25e4994c
rbnrayan <rayan.robion7@gmail.com>
2022-05-20 18:02:56 +0200

    Colorscheme

--- lua/user/colorscheme.lua
+++ lua/user/colorscheme.lua
@@ -1,7 +1,17 @@
-local colorscheme = "plain"
+-- Old way
+-- local colorscheme = "plain"
 
-local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
-if not status_ok then
-  vim.notify("colorscheme " .. colorscheme .. " not found!")
-  return
-end
+-- local status_ok, _ = pcall(vim.cmd, "colorscheme " .. colorscheme)
+-- if not status_ok then
+--   vim.notify("colorscheme " .. colorscheme .. " not found!")
+--   return
+-- end
+
+vim.cmd [[
+try
+  colorscheme plain
+catch /^Vim\%((\a\+)\)\=:E185/
+  colorscheme default
+  set background=dark
+endtry
+]]
