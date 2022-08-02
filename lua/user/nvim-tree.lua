5c584d953922cac7e912fe987d264aea0703e2ab
rbnrayan <rayan.robion7@gmail.com>
2022-08-02 11:56:14 +0200

    remove comments

--- lua/user/nvim-tree.lua
+++ lua/user/nvim-tree.lua
@@ -119,15 +119,4 @@ nvim_tree.setup {
       quit_on_open = false,
     }
   },
-  -- quit_on_open = 0,
-  -- git_hl = 1,
-  -- disable_window_picker = 0,
-  -- root_folder_modifier = ":t",
-  -- show_icons = {
-  --   git = 1,
-  --   folders = 1,
-  --   files = 1,
-  --   folder_arrows = 1,
-  --   tree_width = 30,
-  -- },
 }
